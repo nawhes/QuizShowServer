78246bc8273d6d9e21f9ccc0d8feeaebd7578edb
kjwook <cmdhema@gmail.com>
2019-04-21 23:04:47 +0900

    update for push message

--- scheduler.js
+++ scheduler.js
@@ -1,8 +1,9 @@
 var schedule = require('node-schedule');
 var FCM = require('fcm-node')
-    
-// var serverKey = require('path/to/privatekey.json') //put the generated private key path here    
-// var fcm = new FCM(serverKey);
+
+var User = require('./models').User;
+var serverKey = require('./quizdapp-firebase-adminsdk-9czhf-721317ac64.json') //put the generated private key path here    
+var fcm = new FCM(serverKey);
 
 exports.addSchedule = function (date, kind, prize) {
     var year = date.substring(0, 4);
@@ -11,36 +12,38 @@ exports.addSchedule = function (date, kind, prize) {
     var hour = date.substring(8, 10);
     var min = date.substring(10, 12);
     var sec = date.substring(12, 14);
-    console.log(year + ',' + month + ", " + day + ", " + + min + ", " + sec);
     var date = new Date(year, month - 1, day, hour, min, sec);
-
+    
     schedule.scheduleJob(date, function(){ 
         console.log('퀴즈쇼가 시작됩니다.'); 
-        // sendMessageToDevice();
+        sendMessageToDevice(year + '년 ' + month + '월 ' + day + '일 ' + hour + '시 ' + min + '분 ', kind, prize);
     });
 }
 
-function sendMessageToDevice() {
-    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-        to: 'registration_token', 
-        collapse_key: 'your_collapse_key',
-        
-        notification: {
-            title: 'Title of your push notification', 
-            body: 'Body of your push notification' 
-        },
-        
-        data: {  //you can send only notification or only data(or include both)
-            my_key: 'my value',
-            my_another_key: 'my another value'
+async function sendMessageToDevice(date, kind, prize) {
+    try {
+        const userList = await User.findAll();
+        var tokens = [];
+        for ( var i = 0; i < userList.length; i++ ) {
+            tokens.push(userList[i].deviceToken);
         }
+        console.log(tokens);
+        var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+            registration_ids: tokens,
+            notification: {
+                title: '잠시 후 퀴즈쇼가 시작됩니다!', 
+                body: date + "에 총 " + prize + "개의 " + kind + "가 상품입니다."  
+            },
+        }
+        
+        fcm.send(message, function(err, response){
+            if (err) {
+                console.log("Something has gone wrong!")
+            } else {
+                console.log("Successfully sent with response: ", response)
+            }
+        })
+    } catch(e) {
+        console.log(e);
     }
-    
-    fcm.send(message, function(err, response){
-        if (err) {
-            console.log("Something has gone wrong!")
-        } else {
-            console.log("Successfully sent with response: ", response)
-        }
-    })
 }
