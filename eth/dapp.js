670e862138c5a9f077597b5d53bdb4beeb79b0ee
kjwook <cmdhema@gmail.com>
2019-04-21 20:52:24 +0900

    modify config, eth files for infura

--- eth/dapp.js
+++ eth/dapp.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var async = require('async');
+var tx = require('ethereumjs-tx');
 var content = fs.readFileSync("./config.json");
 const QuizInformation = require('../models').QuizInformation;
 var scheduler = require('../scheduler');
@@ -8,33 +9,48 @@ var contract = JSON.parse(fs.readFileSync("./build/contracts/QuizShowToken.json"
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider(config.eth.url));
 var admin = config.admin;
+var adminPK = Buffer.from(config.adminPK, 'hex');
 var quizContract = new web3.eth.Contract(contract.abi, config.contract.quiz);
 
 // 퀴즈게임 접수 : 일시 20190312120000, 상품 QC, 상금 1000000000, 상태 0)
 //   state  상태 : 0 - 대기, 1 - 게임시작, 2-게임종료, 3- 정산종료
 // ------------------------------------------------------------------------
-var a = 0;
 exports.reserveQuizShow = function (request, callback) {
-    var obj = {
-        from: admin,
-        value: 0,
-        gas: 5000000,
-        gasPrice: '200'
-    }
-    quizContract.methods.reserveQuiz(request.datetime, request.prizeKind, parseInt(request.amount)).send(obj, (err, txid) => {
-        if(!err) {
-            if ( a == 0 ) {
-                a++;
-                // db.reserveQuizShow(request.datetime, request.prizeKind, request.amount);
-                QuizInformation.create({
-                    startDate: request.datetime,
-                    rewardAmount: request.amount,
-                    rewardToken: request.prizeKind
-                })
-                scheduler.addSchedule(request.datetime, request.prizeKind, request.amount);
-                return callback(null);
-            }
+   
+    var contractFunction = quizContract.methods.reserveQuiz(request.datetime, request.prizeKind, parseInt(request.amount));
+    var functionABI = contractFunction.encodeABI();
+
+    web3.eth.getTransactionCount(admin).then(_nonce => {
+        nonce = _nonce.toString(16);
+    
+        console.log("Nonce: " + nonce);
+        var transactionObj = {
+            nonce: '0x' + nonce,
+            from: admin,
+            to: '0xf9da1791376e03da8cf6636aa65cfb72dc77169b',
+            value: 0,
+            data: functionABI,
+            gasLimit: 2100000,
+            gasPrice: 2000000000
         }
+        const transaction = new tx(transactionObj);
+        transaction.sign(adminPK);
+    
+        const serializedTx = transaction.serialize();
+    
+        web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('receipt', receipt => {
+            console.log('RESULT!!!!');
+            console.log(receipt);
+            QuizInformation.create({
+                startDate: request.datetime,
+                rewardAmount: request.amount,
+                rewardToken: request.prizeKind
+            })
+            scheduler.addSchedule(request.datetime, request.prizeKind, request.amount);
+            return callback(null);
+        })
+        .on('error', console.error); 
+
     });
 }
 
